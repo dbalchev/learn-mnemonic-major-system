a9d0ba7f98a4c4ff23000f46fa8c698c2769d973
Daniel Balchev <dbalchev@gmail.com>
2024-08-05 23:27:10 +0300

    Add single-word integration test

--- src/routes/single-word/+page.svelte
+++ src/routes/single-word/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { PEGS } from '$lib/pegs';
 	import Quiz from '$lib/Quiz.svelte';
-
+	import { onMount } from 'svelte';
 	const shuffledPegs = PEGS.map((peg) => ({ ...peg, sortKey: Math.random() })).sort(
 		(lh, rh) => lh.sortKey - rh.sortKey
 	);
-	const items = shuffledPegs.map((peg) => ({ prompt: peg.word, answer: peg.digits }));
+	let items = shuffledPegs.map((peg) => ({ prompt: peg.word, answer: peg.digits }));
+	onMount(() => {
+		if ($page.url.searchParams.get('type') === 'testing') {
+			items = [
+				{ prompt: 'test', answer: '101' },
+				{ prompt: 'spec', answer: '097' }
+			];
+		}
+	});
 </script>
 
 <Quiz allItems={items} />
--- tests/test.ts
+++ tests/test.ts
@@ -5,5 +5,20 @@ test('home page has a link to single word quiz', async ({ page }) => {
 	const singleWordQuizLocator = page.getByText('Single Word quiz');
 	await expect(singleWordQuizLocator).toBeVisible();
 	await singleWordQuizLocator.click();
+	await page.waitForURL(/single-word$/);
+	expect(page.url()).toMatch(/single-word$/);
+});
+
+test('singe word quiz works', async ({ page }) => {
+	await page.goto('/single-word?type=testing');
 	await expect(page.getByText('quiz 0')).toBeVisible();
+	await expect(page.getByText('test')).toBeVisible();
+	await expect(page.getByText('Successes: 0')).toBeVisible();
+	await page.keyboard.type('101');
+	await expect(page.getByText('Successes: 1')).toBeVisible();
+	await expect(page.getByText('spec')).toBeVisible();
+	await expect(page.getByText('Fails: 0')).toBeVisible();
+	await page.keyboard.type('098');
+	await expect(page.getByText('Fails: 1')).toBeVisible();
+	await expect(page.getByText('no more quiz items')).toBeVisible();
 });
